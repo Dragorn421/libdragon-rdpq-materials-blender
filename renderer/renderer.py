5a17e218e6fc94c2670121a131105f584033bac5
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-02 20:46:43 +0100

    slight refactor

--- renderer/renderer.py
+++ renderer/renderer.py
@@ -1,9 +1,12 @@
 import dataclasses
 from pathlib import Path
-from typing import Optional, Sequence
+from typing import Optional, Sequence, TYPE_CHECKING
 
 import bpy
 import mathutils
+
+if TYPE_CHECKING:
+    import gpu
 
 import numpy as np
 
@@ -57,6 +60,210 @@ COMBINER_MAP = {
 }
 
 
+def get_material_data(mat: bpy.types.Material):
+    mat_rdpq = util.LIBDRAGON_RDPQ(mat)
+    tex0 = mat_rdpq.texture0.image if mat_rdpq.use_texture0 else None
+    combiner_words = [0, 0, 0, 0]
+    for slot, shift, word in (
+        (
+            mat_rdpq.combiner.rgb_A_0,
+            magic.COMBINER_RGB_2A_SUBA_SHIFT,
+            magic.COMBINER_RGB_2A_SUBA_WORD,
+        ),
+        (
+            mat_rdpq.combiner.rgb_B_0,
+            magic.COMBINER_RGB_2A_SUBB_SHIFT,
+            magic.COMBINER_RGB_2A_SUBB_WORD,
+        ),
+        (
+            mat_rdpq.combiner.rgb_C_0,
+            magic.COMBINER_RGB_2A_MUL_SHIFT,
+            magic.COMBINER_RGB_2A_MUL_WORD,
+        ),
+        (
+            mat_rdpq.combiner.rgb_D_0,
+            magic.COMBINER_RGB_2A_ADD_SHIFT,
+            magic.COMBINER_RGB_2A_ADD_WORD,
+        ),
+        (
+            mat_rdpq.combiner.rgb_A_1,
+            magic.COMBINER_RGB_2B_SUBA_SHIFT,
+            magic.COMBINER_RGB_2B_SUBA_WORD,
+        ),
+        (
+            mat_rdpq.combiner.rgb_B_1,
+            magic.COMBINER_RGB_2B_SUBB_SHIFT,
+            magic.COMBINER_RGB_2B_SUBB_WORD,
+        ),
+        (
+            mat_rdpq.combiner.rgb_C_1,
+            magic.COMBINER_RGB_2B_MUL_SHIFT,
+            magic.COMBINER_RGB_2B_MUL_WORD,
+        ),
+        (
+            mat_rdpq.combiner.rgb_D_1,
+            magic.COMBINER_RGB_2B_ADD_SHIFT,
+            magic.COMBINER_RGB_2B_ADD_WORD,
+        ),
+    ):
+        combiner_words[word] |= COMBINER_MAP[slot] << shift
+    mat_data = MaterialData(tex0, combiner_words)
+    return mat_data
+
+
+def get_mesh_data(obj: bpy.types.Object, depsgraph: bpy.types.Depsgraph):
+    meshes: list[MeshData] = []
+
+    obj_eval = obj.evaluated_get(depsgraph)
+    mesh = obj_eval.to_mesh()
+    mesh.calc_loop_triangles()
+    mesh.calc_normals_split()
+
+    model_matrix = obj.matrix_world
+
+    vertices_co = np.empty((len(mesh.vertices), 3), dtype=np.float32)
+    mesh.vertices.foreach_get("co", vertices_co.ravel())
+
+    loop_triangles_loops = np.empty((len(mesh.loop_triangles), 3), dtype=np.int32)
+    mesh.loop_triangles.foreach_get("loops", loop_triangles_loops.ravel())
+
+    loops_normal = np.empty((len(mesh.loops), 3), dtype=np.float32)
+    mesh.loops.foreach_get("normal", loops_normal.ravel())
+
+    loops_vertex_index = np.empty(len(mesh.loops), dtype=np.int32)
+    mesh.loops.foreach_get("vertex_index", loops_vertex_index)
+
+    active_color_attribute = mesh.color_attributes.active_color
+    if active_color_attribute is None:
+        loops_color = np.ones((len(mesh.loops), 4), dtype=np.float32)
+    else:
+        if active_color_attribute.data_type in {
+            "FLOAT_COLOR",
+            "BYTE_COLOR",
+        }:
+            assert isinstance(
+                active_color_attribute,
+                (
+                    bpy.types.FloatColorAttribute,
+                    bpy.types.ByteColorAttribute,
+                ),
+            )
+            # Note: for ByteColorAttribute too the color uses floats
+            if active_color_attribute.domain == "CORNER":
+                loops_color = np.empty((len(mesh.loops), 4), dtype=np.float32)
+                active_color_attribute.data.foreach_get("color", loops_color.ravel())
+            elif active_color_attribute.domain == "POINT":
+                vertices_color = np.empty((len(mesh.vertices), 4), dtype=np.float32)
+                active_color_attribute.data.foreach_get("color", vertices_color.ravel())
+                loops_color = vertices_color[loops_vertex_index, :]
+            else:
+                raise NotImplementedError(active_color_attribute.domain)
+        else:
+            raise NotImplementedError(active_color_attribute.data_type)
+
+    active_uv_layer = mesh.uv_layers.active
+    if active_uv_layer is None:
+        loops_uv = None
+    else:
+        loops_uv = np.empty((len(mesh.loops), 2), dtype=np.float32)
+        active_uv_layer.data.foreach_get("uv", loops_uv.ravel())
+
+    loop_triangles_material_index = np.empty(len(mesh.loop_triangles), dtype=np.int32)
+    mesh.loop_triangles.foreach_get(
+        "material_index", loop_triangles_material_index.ravel()
+    )
+
+    loops_co = vertices_co[loops_vertex_index, :]
+
+    mesh.free_normals_split()
+    obj_eval.to_mesh_clear()
+
+    mat_mask = np.empty_like(loop_triangles_material_index, dtype=bool)
+
+    for mat_index in np.unique(loop_triangles_material_index):
+        mat_index: int
+        if mat_index < len(obj.material_slots):
+            mat = obj.material_slots[mat_index].material
+        else:
+            mat = None
+
+        if mat is not None:
+            mat_data = get_material_data(mat)
+        else:
+            mat_data = None
+
+        # Get the loops indices for the triangles using mat
+        np.equal(loop_triangles_material_index, mat_index, out=mat_mask)
+        mat_triangles_loops = loop_triangles_loops[mat_mask, :]
+
+        # Get the loops used by the mat-using triangles,
+        # for subsetting the coordinates/normal/color buffers,
+        # and also the triangles loops indices in that subset of triangles.
+        mat_used_loops, mat_triangles_loops_used_loops = np.unique(
+            mat_triangles_loops, return_inverse=True
+        )
+        mat_triangles_loops_used_loops = mat_triangles_loops_used_loops.reshape(
+            mat_triangles_loops.shape
+        ).astype(np.int32)
+
+        meshes.append(
+            MeshData(
+                model_matrix,
+                mat_triangles_loops_used_loops,
+                loops_co[mat_used_loops, :],
+                loops_normal[mat_used_loops, :],
+                loops_color[mat_used_loops, :],
+                (loops_uv[mat_used_loops, :] if loops_uv is not None else None),
+                mat_data,
+            )
+        )
+
+    return meshes
+
+
+def draw_mesh(
+    mesh: MeshData,
+    shader: "gpu.types.GPUShader",
+    proj_view_mtx: mathutils.Matrix,
+    view_mtx: mathutils.Matrix,
+):
+    import gpu
+    import gpu_extras
+
+    shader.uniform_float("matMVP", proj_view_mtx @ mesh.model_matrix)
+    shader.uniform_float("matMV", view_mtx @ mesh.model_matrix)
+
+    valid_inputs_flags = 0
+
+    if mesh.material is not None:
+        mat = mesh.material
+        if mat.tex0 is not None:
+            valid_inputs_flags |= magic.VALID_IN_TEX0
+            shader.uniform_sampler("inTex0", gpu.texture.from_image(mat.tex0))
+        valid_inputs_flags |= magic.VALID_IN_COMBINER
+        shader.uniform_int("inCombiner", mat.combiner_words)
+
+    content = {
+        "inPos": mesh.loops_co,
+        "inNormal": mesh.loops_normal,
+        "inColor": mesh.loops_color,
+    }
+
+    if mesh.loops_uv is not None:
+        valid_inputs_flags |= magic.VALID_IN_UV
+        content["inUV"] = mesh.loops_uv
+
+    shader.uniform_int("inValidInputs", valid_inputs_flags)
+
+    batch: gpu.types.GPUBatch = gpu_extras.batch.batch_for_shader(
+        shader,
+        "TRIS",
+        content,
+        indices=mesh.loop_triangles_loops,
+    )
+    batch.draw(shader)
+
+
 class RDPQMaterialsRenderEngine(bpy.types.RenderEngine):
     bl_idname = "LIBDRAGON_RDPQ_MATERIALS"
     bl_label = "libdragon RDPQ materials"
@@ -78,175 +285,7 @@ class RDPQMaterialsRenderEngine(bpy.types.RenderEngine):
 
         for obj in scene.objects:
             if obj.type == "MESH":
-                obj_eval = obj.evaluated_get(depsgraph)
-                mesh = obj_eval.to_mesh()
-                mesh.calc_loop_triangles()
-                mesh.calc_normals_split()
-
-                model_matrix = obj.matrix_world
-
-                vertices_co = np.empty((len(mesh.vertices), 3), dtype=np.float32)
-                mesh.vertices.foreach_get("co", vertices_co.ravel())
-
-                loop_triangles_loops = np.empty(
-                    (len(mesh.loop_triangles), 3), dtype=np.int32
-                )
-                mesh.loop_triangles.foreach_get("loops", loop_triangles_loops.ravel())
-
-                loops_normal = np.empty((len(mesh.loops), 3), dtype=np.float32)
-                mesh.loops.foreach_get("normal", loops_normal.ravel())
-
-                loops_vertex_index = np.empty(len(mesh.loops), dtype=np.int32)
-                mesh.loops.foreach_get("vertex_index", loops_vertex_index)
-
-                active_color_attribute = mesh.color_attributes.active_color
-                if active_color_attribute is None:
-                    loops_color = np.ones((len(mesh.loops), 4), dtype=np.float32)
-                else:
-                    if active_color_attribute.data_type in {
-                        "FLOAT_COLOR",
-                        "BYTE_COLOR",
-                    }:
-                        assert isinstance(
-                            active_color_attribute,
-                            (
-                                bpy.types.FloatColorAttribute,
-                                bpy.types.ByteColorAttribute,
-                            ),
-                        )
-                        # Note: for ByteColorAttribute too the color uses floats
-                        if active_color_attribute.domain == "CORNER":
-                            loops_color = np.empty(
-                                (len(mesh.loops), 4), dtype=np.float32
-                            )
-                            active_color_attribute.data.foreach_get(
-                                "color", loops_color.ravel()
-                            )
-                        elif active_color_attribute.domain == "POINT":
-                            vertices_color = np.empty(
-                                (len(mesh.vertices), 4), dtype=np.float32
-                            )
-                            active_color_attribute.data.foreach_get(
-                                "color", vertices_color.ravel()
-                            )
-                            loops_color = vertices_color[loops_vertex_index, :]
-                        else:
-                            raise NotImplementedError(active_color_attribute.domain)
-                    else:
-                        raise NotImplementedError(active_color_attribute.data_type)
-
-                active_uv_layer = mesh.uv_layers.active
-                if active_uv_layer is None:
-                    loops_uv = None
-                else:
-                    loops_uv = np.empty((len(mesh.loops), 2), dtype=np.float32)
-                    active_uv_layer.data.foreach_get("uv", loops_uv.ravel())
-
-                loop_triangles_material_index = np.empty(
-                    len(mesh.loop_triangles), dtype=np.int32
-                )
-                mesh.loop_triangles.foreach_get(
-                    "material_index", loop_triangles_material_index.ravel()
-                )
-
-                loops_co = vertices_co[loops_vertex_index, :]
-
-                mesh.free_normals_split()
-                obj_eval.to_mesh_clear()
-
-                mat_mask = np.empty_like(loop_triangles_material_index, dtype=bool)
-
-                for mat_index in np.unique(loop_triangles_material_index):
-                    mat_index: int
-                    if mat_index < len(obj.material_slots):
-                        mat = obj.material_slots[mat_index].material
-                    else:
-                        mat = None
-
-                    if mat is not None:
-                        mat_rdpq = util.LIBDRAGON_RDPQ(mat)
-                        tex0 = (
-                            mat_rdpq.texture0.image if mat_rdpq.use_texture0 else None
-                        )
-                        combiner_words = [0, 0, 0, 0]
-                        for slot, shift, word in (
-                            (
-                                mat_rdpq.combiner.rgb_A_0,
-                                magic.COMBINER_RGB_2A_SUBA_SHIFT,
-                                magic.COMBINER_RGB_2A_SUBA_WORD,
-                            ),
-                            (
-                                mat_rdpq.combiner.rgb_B_0,
-                                magic.COMBINER_RGB_2A_SUBB_SHIFT,
-                                magic.COMBINER_RGB_2A_SUBB_WORD,
-                            ),
-                            (
-                                mat_rdpq.combiner.rgb_C_0,
-                                magic.COMBINER_RGB_2A_MUL_SHIFT,
-                                magic.COMBINER_RGB_2A_MUL_WORD,
-                            ),
-                            (
-                                mat_rdpq.combiner.rgb_D_0,
-                                magic.COMBINER_RGB_2A_ADD_SHIFT,
-                                magic.COMBINER_RGB_2A_ADD_WORD,
-                            ),
-                            (
-                                mat_rdpq.combiner.rgb_A_1,
-                                magic.COMBINER_RGB_2B_SUBA_SHIFT,
-                                magic.COMBINER_RGB_2B_SUBA_WORD,
-                            ),
-                            (
-                                mat_rdpq.combiner.rgb_B_1,
-                                magic.COMBINER_RGB_2B_SUBB_SHIFT,
-                                magic.COMBINER_RGB_2B_SUBB_WORD,
-                            ),
-                            (
-                                mat_rdpq.combiner.rgb_C_1,
-                                magic.COMBINER_RGB_2B_MUL_SHIFT,
-                                magic.COMBINER_RGB_2B_MUL_WORD,
-                            ),
-                            (
-                                mat_rdpq.combiner.rgb_D_1,
-                                magic.COMBINER_RGB_2B_ADD_SHIFT,
-                                magic.COMBINER_RGB_2B_ADD_WORD,
-                            ),
-                        ):
-                            combiner_words[word] |= COMBINER_MAP[slot] << shift
-                        mat_data = MaterialData(tex0, combiner_words)
-                    else:
-                        mat_data = None
-
-                    # Get the loops indices for the triangles using mat
-                    np.equal(loop_triangles_material_index, mat_index, out=mat_mask)
-                    mat_triangles_loops = loop_triangles_loops[mat_mask, :]
-
-                    # Get the loops used by the mat-using triangles,
-                    # for subsetting the coordinates/normal/color buffers,
-                    # and also the triangles loops indices in that subset of triangles.
-                    mat_used_loops, mat_triangles_loops_used_loops = np.unique(
-                        mat_triangles_loops, return_inverse=True
-                    )
-                    mat_triangles_loops_used_loops = (
-                        mat_triangles_loops_used_loops.reshape(
-                            mat_triangles_loops.shape
-                        ).astype(np.int32)
-                    )
-
-                    meshes.append(
-                        MeshData(
-                            model_matrix,
-                            mat_triangles_loops_used_loops,
-                            loops_co[mat_used_loops, :],
-                            loops_normal[mat_used_loops, :],
-                            loops_color[mat_used_loops, :],
-                            (
-                                loops_uv[mat_used_loops, :]
-                                if loops_uv is not None
-                                else None
-                            ),
-                            mat_data,
-                        )
-                    )
+                meshes.extend(get_mesh_data(obj, depsgraph))
 
         self.scene_data = SceneData(meshes)
 
@@ -283,46 +322,12 @@ class RDPQMaterialsRenderEngine(bpy.types.RenderEngine):
 
         self.shader.bind()
         for mesh in self.scene_data.meshes:
-            self.shader.uniform_float(
-                "matMVP",
-                context.region_data.perspective_matrix @ mesh.model_matrix,
-            )
-            self.shader.uniform_float(
-                "matMV",
-                context.region_data.view_matrix @ mesh.model_matrix,
-            )
-
-            valid_inputs_flags = 0
-
-            if mesh.material is not None:
-                mat = mesh.material
-                if mat.tex0 is not None:
-                    valid_inputs_flags |= magic.VALID_IN_TEX0
-                    self.shader.uniform_sampler(
-                        "inTex0", gpu.texture.from_image(mat.tex0)
-                    )
-                valid_inputs_flags |= magic.VALID_IN_COMBINER
-                self.shader.uniform_int("inCombiner", mat.combiner_words)
-
-            content = {
-                "inPos": mesh.loops_co,
-                "inNormal": mesh.loops_normal,
-                "inColor": mesh.loops_color,
-            }
-
-            if mesh.loops_uv is not None:
-                valid_inputs_flags |= magic.VALID_IN_UV
-                content["inUV"] = mesh.loops_uv
-
-            self.shader.uniform_int("inValidInputs", valid_inputs_flags)
-
-            batch: gpu.types.GPUBatch = gpu_extras.batch.batch_for_shader(
+            draw_mesh(
+                mesh,
                 self.shader,
-                "TRIS",
-                content,
-                indices=mesh.loop_triangles_loops,
+                context.region_data.perspective_matrix,
+                context.region_data.view_matrix,
             )
-            batch.draw(self.shader)
 
 
 def get_compatible_panels():
